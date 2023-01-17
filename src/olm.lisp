a4483af16c4d1a9bbd9d562ae48941cfa97bca9c
Josh <k1llfrenzy@Hotmail.co.uk>
2023-01-17 15:06:30 +0000

    olm version update changed search path to lib64

--- src/olm.lisp
+++ src/olm.lisp
@@ -5,8 +5,9 @@
 (cffi:define-foreign-library libolm
   (:unix (:or "libolm.so"
               "libolm.so.3"
-              "libolm.so.3.2.4")
-   :search-path "/usr/local/lib/")
+              "libolm.so.3.2.4"
+              "libolm.so.3.2.14")
+   :search-path "/usr/local/lib64/")
   (t (:default "libolm")))
 
 (cffi:use-foreign-library libolm)
